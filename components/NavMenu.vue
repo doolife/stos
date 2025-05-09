fb6d9c5beba96872211e7e567e741df3124653d7
es161114 <es161114@ncsoft.com>
2025-05-09 17:12:24 +0900

    동적 라우팅 test

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -3,6 +3,8 @@
     <ul>
       <li><NuxtLink to="/">홈</NuxtLink></li>
       <li><NuxtLink to="/about">어바웃</NuxtLink></li>
+      <li><NuxtLink to="/posts/1">게시물 1</NuxtLink></li>
+      <li><NuxtLink to="/posts/2">게시물 2</NuxtLink></li>
     </ul>
   </nav>
 </template>
@@ -15,3 +17,5 @@ nav ul {
   padding: 0;
 }
 </style>
+<script setup lang="ts">
+</script>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>게시물 ID: {{ id }}</h1>
+    <pre>{{ post }}</pre>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const id = route.params.id
+
+const { data: post } = await useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+</script>
